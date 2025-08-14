0c6c482f7bcb7f6f82ea06358ad1212c7a3da19b
RalphWangoto <wangotoraphael@gmail.com>
2025-08-14 17:13:23 +0300

    Setup: Add TestNG Framework, Initialize chrome and access web application.

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -1,7 +1,26 @@
 package tests;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class FirstTest {
-	public void openGoogleHomePage() {
+	public static WebDriver driver;
+	String url = "https://rahulshettyacademy.com/seleniumPractise/#/";
 	
+	@Test(priority = 1, enabled = true)
+	public void openGoogleHomePage() {
+		
+		// Initialize chrome driver
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		
+		// Get URL
+		driver.get(url);
+		
+		driver.quit();
 	}
 }
